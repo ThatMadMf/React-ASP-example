b53e4d2c918c10b40b6316bd2dcdf823bdf429f6
Onoshko Max <maksymon@tahometer.com>
2020-09-17 17:06:35 +0300

    Remove unused models

--- client/src/components/Navigation/Navigation.model.ts
+++
@@ -1,6 +0,0 @@
-import NavigationItemModel from "./NavigationItem.model";
-
-export default interface NavigationModel {
-    title: string;
-    items: NavigationItemModel[];
-}
--- client/src/components/Navigation/NavigationItem.model.ts
+++
@@ -1,4 +0,0 @@
-export default interface NavigationItemModel {
-    text: string;
-    link: string;
-}
